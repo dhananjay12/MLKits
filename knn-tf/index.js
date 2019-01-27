ce903596292da899bb196d193e01f96777cd3dfe
Dhananjay Singh <dhananjay1204@gmail.com>
2019-01-27 18:54:51 +0530

    knn-tf-1

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -1,17 +1,18 @@
-require('@tensorflow/tfjs-node');
-const tf = require('@tensorflow/tfjs');
-const loadCSV = require('./load-csv');
+require("@tensorflow/tfjs-node");
+const tf = require("@tensorflow/tfjs");
+const loadCSV = require("./load-csv");
 
-let {
-    features,
-    labels,
-    testFeatures,
-    testLabels
-} = loadCSV(
-    'kc_house_data.csv', {
-        shuffle: true,
-        splitTest: 10,
-        dataColumns: ['lat', 'long', 'sqft_lot', 'sqft_living'],
-        labelColumns: ['price']
-    }
+let { features, labels, testFeatures, testLabels } = loadCSV(
+  "kc_house_data.csv",
+  {
+    shuffle: true,
+    splitTest: 10,
+    dataColumns: ["lat", "long", "sqft_lot", "sqft_living"],
+    labelColumns: ["price"]
+  }
 );
+
+console.log("features", features);
+console.log("labels", labels);
+console.log("testFeatures", testFeatures);
+console.log("testLabels", testLabels);
